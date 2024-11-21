75be2a448af3a0247493f58a4c73133f766d62f0
Yanggeonmo454 <didrjsah05@naver.com>
2024-11-21 14:26:35 +0900

    1st commit

---
+++ 1120/index.py
@@ -0,0 +1,90 @@
+"""
+print("안녕하세요")
+print("hello", "python")
+print("hello", "python", sep=" ")
+print("안녕하세요", end="/")
+print("저는 양건모입니다")
+print(1234)
+"""
+""""
+singer = input("좋아하는 가수는? ")
+print("좋아하는 가수는 ", singer, "입니다.")
+
+
+x = 10
+print("before x", id(x))
+
+import keyword
+y, z = 3.14, "안녕하세요"
+print("y", y)
+print("z", z)
+
+x = "반갑습니다"
+print("after x", id(x))
+
+a = [1, 2, 3]
+print("before", a, id(a))
+a.append(4)
+print("after a", a, id(a))
+
+
+print(keyword.kwlist)
+
+x = 48 / 2 * (9+3)
+print(x)
+
+
+x = (100 - 2) / 7 + 9 * 3
+print(x)
+
+
+num = 5
+num += 5
+print("+=", num)
+num -= 2
+print("-=", num)
+num *= 6
+print("*=", num)
+num /= 2
+print("/=", num)
+num //= 3
+print("//=", num)
+num %= 3
+print("%=", num)
+num **= 4
+print("**=", num)
+
+
+num1 = 10
+num2 = 20
+num3 = "10"
+
+print(num1 > num2)
+print(num1 < num2)
+print(num1 == num3)
+print(num1 >= num2)
+print(num1 != num3)
+
+
+a = 2 > 1
+b = 3 < 1
+c = 1 == 1
+d = 3 >= 4
+
+print(a and c)
+
+print(b or c)
+
+print(b or d)
+
+print(not a)
+
+
+a = "hello world"
+print(a in "H")
+
+num = 14
+a = num % 2
+
+print("True면 짝수, False면 홀수:", a == 0)
+"""
